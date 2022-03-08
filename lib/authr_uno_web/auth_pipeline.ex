2fe09b9713a13a741653b847c051926d8abfb845
Efen Stakes <efenstakes101@gmail.com>
2022-03-08 19:54:06 +0300

    remove guardian files

--- lib/authr_uno_web/auth_pipeline.ex
+++
@@ -1,9 +0,0 @@
-defmodule AuthrUno.Guardian.AuthPipeline do
-  use Guardian.Plug.Pipeline, otp_app: :authr_uno,
-          module: AuthrUno.Guardian,
-          error_handler: AuthrUno.AuthErrorHandler
-
-  plug Guardian.Plug.VerifyHeader, realm: "Bearer"
-  plug Guardian.Plug.EnsureAuthenticated
-  plug Guardian.Plug.LoadResource
-end
